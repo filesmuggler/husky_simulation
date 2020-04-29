aa12a1b881b5234e8ed8185d5979306d40727f4f
filesmuggler <krzste09@outlook.com>
2020-04-29 12:59:06 +0200

    ur_d435_ws added

---
+++ ur_d435_ws/build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/krzysztof/Repos/husky_simulation/ur_d435_ws/src
+# Build directory: /home/krzysztof/Repos/husky_simulation/ur_d435_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("realsense-ros/realsense2_description")
+subdirs("ur5/ur5_description")
+subdirs("ur5/ur5_gazebo")
+subdirs("robotiq/robotiq_description")
+subdirs("realsense-ros/realsense2_camera")
+subdirs("robotiq/robotiq_gazebo")
---
+++ ur_d435_ws/devel/share/realsense2_camera/cmake/realsense2_camera-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(realsense2_camera_MESSAGE_FILES "/home/krzysztof/Repos/husky_simulation/ur_d435_ws/src/realsense-ros/realsense2_camera/msg/IMUInfo.msg;/home/krzysztof/Repos/husky_simulation/ur_d435_ws/src/realsense-ros/realsense2_camera/msg/Extrinsics.msg")
+set(realsense2_camera_SERVICE_FILES "")
