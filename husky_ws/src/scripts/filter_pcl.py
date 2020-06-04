ba55da540a4260647438b972c51f951409890855
filesmuggler <krzste09@outlook.com>
2020-06-04 09:29:49 +0200

    b4 messing filter_plc.py

--- husky_ws/src/scripts/filter_pcl.py
+++ husky_ws/src/scripts/filter_pcl.py
@@ -12,10 +12,13 @@ def callback(data):
     # points[:,0] = pc['x']
     # points[:,1] = pc['y']
     # points[:,2] = pc['z']
-
+    msg = PointCloud2()
     for point in sensor_msgs.point_cloud2.read_points(data, skip_nans=True):
         print(point[0],point[1],point[2])
-    pass
+        msg.append(point)
+    
+    
+
 
 def filter_pcl():
     rospy.init_node('filter_pcl',anonymous=True)
