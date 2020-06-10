8a546a6362ea0e995b05f21bf58cdd3145963155
filesmuggler <krzste09@outlook.com>
2020-06-10 12:58:49 +0200

    grabbing needs fixing

---
+++ husky_ws/src/scripts/give_coord.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+import sensor_msgs.point_cloud2
+from sensor_msgs.msg import PointCloud2
+import ros_numpy
+import numpy as np
+import math
+
+from tf.transformations import quaternion_from_euler
+import geometry_msgs.msg
+
+
+def pointcloud2_to_array(cloud_msg, squeeze=True):
+    ''' Converts a rospy PointCloud2 message to a numpy recordarray
+    Reshapes the returned array to have shape (height, width), even if the height is 1.
+    The reason for using np.fromstring rather than struct.unpack is speed... especially
+    for large point clouds, this will be <much> faster.
+    '''
+    # construct a numpy record type equivalent to the point type of this cloud
+    dtype_list = ros_numpy.point_cloud2.fields_to_dtype(cloud_msg.fields, cloud_msg.point_step)
+
+    # parse the cloud into an array
+    cloud_arr = np.fromstring(cloud_msg.data, dtype_list)
+
+    # remove the dummy fields that were added
+    cloud_arr = cloud_arr[
+        [fname for fname, _type in dtype_list if not (fname[:len(ros_numpy.point_cloud2.DUMMY_FIELD_PREFIX)] == ros_numpy.point_cloud2.DUMMY_FIELD_PREFIX)]]
+
+    if squeeze and cloud_msg.height == 1:
+        return np.reshape(cloud_arr, (cloud_msg.width,))
+    else:
+        return np.reshape(cloud_arr, (cloud_msg.height, cloud_msg.width)) 
+
+def callback(data):
+    pcl2_array = pointcloud2_to_array(data)
+    pcl2_new_array = []
+    for point in pcl2_array:
+        if point[2] > 0.0 and point[0]<0.5 and point[0]>-0.5 and point[1]<0.5 and point[1]>-0.5 or point[2]<0.10:
+            pass
+        else:
+            pcl2_new_array.append(point)
+
+    avg_obj_center = [0,0,0]
+    if len(pcl2_new_array) != 0:
+        for point in pcl2_new_array:
+            avg_obj_center[0]=avg_obj_center[0]+point[0]
+            avg_obj_center[1]=avg_obj_center[0]+point[1]
+            avg_obj_center[2]=avg_obj_center[0]+point[2]
+
+        if(len(pcl2_new_array) is not 0):
+            avg_obj_center[0]=avg_obj_center[0]*1.0/len(pcl2_new_array)
+            avg_obj_center[1]=avg_obj_center[1]*1.0/len(pcl2_new_array)
+            avg_obj_center[2]=avg_obj_center[2]*1.0/len(pcl2_new_array)
+
+            print("center: ",avg_obj_center)
+            r = 0.7
+            print(math.sqrt((abs(avg_obj_center[0])+0.22)**2+avg_obj_center[1]**2))
+            if math.sqrt((abs(avg_obj_center[0])-0.22)**2+avg_obj_center[1]**2) > r:
+                print("object to far")
+
+        roll=1.54
+        pitch=0
+        yaw=3.14
+        q = quaternion_from_euler(roll,pitch,yaw)
+        pose = geometry_msgs.msg.Pose()
+        pose.position.x = avg_obj_center[0]
+        pose.position.y = avg_obj_center[1]
+        pose.position.z = avg_obj_center[2]
+        pose.orientation.x = q[0]
+        pose.orientation.y = q[1]
+        pose.orientation.z = q[2]
+        pose.orientation.w = q[3]
+
+        
+        pub = rospy.Publisher("/objectcoord",geometry_msgs.msg.Pose,queue_size=10)
+        while not rospy.is_shutdown():
+            pub.publish(pose)
+            rospy.Rate(2).sleep()
+        
+    else:
+        print("no object within reach of the camera")
+
+
+
+def main():
+    try:
+        rospy.init_node('filter_pcl',anonymous=True)
+        rospy.Subscriber("/ird435/octomap_point_cloud_centers",PointCloud2,callback)
+        rospy.spin()
+        
+
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
+
+if __name__ == '__main__':
+    main()
--- husky_ws/src/scripts/grab_object.py
+++ husky_ws/src/scripts/grab_object.py
@@ -12,102 +12,126 @@ from moveit_commander.conversions import pose_to_list
 import tf
 
 
-def all_close(goal, actual, tolerance):
-  """
-  Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
-  @param: goal       A list of floats, a Pose or a PoseStamped
-  @param: actual     A list of floats, a Pose or a PoseStamped
-  @param: tolerance  A float
-  @returns: bool
-  """
-  all_equal = True
-  if type(goal) is list:
-    for index in range(len(goal)):
-      if abs(actual[index] - goal[index]) > tolerance:
-        return False
-
-  elif type(goal) is geometry_msgs.msg.PoseStamped:
-    return all_close(goal.pose, actual.pose, tolerance)
-
-  elif type(goal) is geometry_msgs.msg.Pose:
-    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
-
-  return True
-
-class MoveGroupPythonIntefaceTutorial(object):
-  def __init__(self):
-    super(MoveGroupPythonIntefaceTutorial, self).__init__()
-
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
-
-    robot = moveit_commander.RobotCommander()
-
-    scene = moveit_commander.PlanningSceneInterface()
-
-    group_name = "manipulator"
-    move_group = moveit_commander.MoveGroupCommander(group_name)
-
-    display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=20)
-
-    planning_frame = move_group.get_planning_frame()
+def callback(data):
+    move_group = self.move_group     
     
-    eef_link = move_group.get_end_effector_link()
-
-    group_names = robot.get_group_names()
-    robot.get_current_state()
-
-    # Misc variables    
-    self.robot = robot
-    self.scene = scene
-    self.move_group = move_group
-    self.display_trajectory_publisher = display_trajectory_publisher
-    self.planning_frame = planning_frame
-    self.eef_link = eef_link
-    self.group_names = group_names
-
-  def go_to_pose_goal(self):
-    move_group = self.move_group
-    pose_goal = geometry_msgs.msg.Pose()
-    
-    ## euler for unplug
-    roll=1.50
-    pitch=0.00
-    yaw=0.0
-    ## pose for unplug
-    pose_goal.position.x = 0.50
-    pose_goal.position.y = -0.50
-    pose_goal.position.z = 0.2
-
-    quaternion = tf.transformations.quaternion_from_euler(roll,pitch,yaw)
-    
-    pose_goal.orientation.x = quaternion[0]
-    pose_goal.orientation.y = quaternion[1]
-    pose_goal.orientation.z = quaternion[2]
-    pose_goal.orientation.w = quaternion[3]
-    
-    move_group.set_pose_target(pose_goal)
+    move_group.set_pose_target(data)
 
     plan = move_group.go(wait=True)
     move_group.stop()
     move_group.clear_pose_targets()
     current_pose = self.move_group.get_current_pose().pose
-    return all_close(pose_goal, current_pose, 0.01)
+    return all_close(data, current_pose, 0.01)
 
+def all_close(goal, actual, tolerance):
+    """
+    Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
+    @param: goal       A list of floats, a Pose or a PoseStamped
+    @param: actual     A list of floats, a Pose or a PoseStamped
+    @param: tolerance  A float
+    @returns: bool
+    """
+    all_equal = True
+    if type(goal) is list:
+      for index in range(len(goal)):
+        if abs(actual[index] - goal[index]) > tolerance:
+          return False
 
+    elif type(goal) is geometry_msgs.msg.PoseStamped:
+      return all_close(goal.pose, actual.pose, tolerance)
+
+    elif type(goal) is geometry_msgs.msg.Pose:
+      return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
+
+    return True
+
+class MoveGroupPythonIntefaceTutorial(object):
+    def __init__(self):
+      super(MoveGroupPythonIntefaceTutorial, self).__init__()
+
+      moveit_commander.roscpp_initialize(sys.argv)
+      rospy.init_node('move_group_python_interface_tutorial', anonymous=True)
+      rospy.Subscriber("/objectcoord",geometry_msgs.msg.Pose,callback)
+      rospy.spin()
+
+      robot = moveit_commander.RobotCommander()
+
+      scene = moveit_commander.PlanningSceneInterface()
+
+      group_name = "manipulator"
+      move_group = moveit_commander.MoveGroupCommander(group_name)
+
+      display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=20)
+      
+      planning_frame = move_group.get_planning_frame()
+      
+      eef_link = move_group.get_end_effector_link()
+
+      group_names = robot.get_group_names()
+      robot.get_current_state()
+
+      # Misc variables    
+      self.robot = robot
+      self.scene = scene
+      self.move_group = move_group
+      self.display_trajectory_publisher = display_trajectory_publisher
+      self.planning_frame = planning_frame
+      self.eef_link = eef_link
+      self.group_names = group_names
+
+    def go_to_pose_goal(self):
+      move_group = self.move_group
+      #pose_goal = geometry_msgs.msg.Pose()
+      
+      ## euler for plug
+      # roll=1.54
+      # pitch=0
+      # yaw=3.14
+      # ## pose for plug
+      # pose_goal.position.x = 0.30
+      # pose_goal.position.y = 0.70
+      # pose_goal.position.z = 0.15
+      ## euler for unplug
+      # roll=1.54
+      # pitch=0
+      # yaw=0
+      # ## pose for unplug
+      # pose_goal.position.x = 0.30
+      # pose_goal.position.y = -0.60
+      # pose_goal.position.z = 0.15
+
+      # quaternion = tf.transformations.quaternion_from_euler(roll,pitch,yaw)
+
+      global pose
+      print(pose)
+      
+      
+      # pose.orientation.x = quaternion[0]
+      # pose.orientation.y = quaternion[1]
+      # pose.orientation.z = quaternion[2]
+      # pose.orientation.w = quaternion[3]
+      
+      move_group.set_pose_target(pose)
+
+      plan = move_group.go(wait=True)
+      move_group.stop()
+      move_group.clear_pose_targets()
+      current_pose = self.move_group.get_current_pose().pose
+      return all_close(pose, current_pose, 0.01)
 
 def main():
-  try:
-    print("Grabing object...")
-    tutorial = MoveGroupPythonIntefaceTutorial()
-    print "============ Press `Enter` to execute a movement using a pose goal ..."
-    raw_input()
-    tutorial.go_to_pose_goal()
+    try:
+        print("Grabing object...")
+        tutorial = MoveGroupPythonIntefaceTutorial()
+        #print "============ Press `Enter` to execute a movement using a pose goal ..."
+        #raw_input()
+        #tutorial.go_to_pose_goal()
+        
 
-  except rospy.ROSInterruptException:
-    return
-  except KeyboardInterrupt:
-    return
+    except rospy.ROSInterruptException:
+        return
+    except KeyboardInterrupt:
+        return
 
 if __name__ == '__main__':
-  main()
+    main()
